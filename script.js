2a3023929077f089600e0ec62b0d7161f4ec848c
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-09-12 20:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function volume_sphere() {
     //Write your code here
+	const radius = parseFloat(document.querySelector("#radius").value);
+	console.log(radius); 
+	const volume = document.querySelector("#volume");
+	let ans = (3.14*radius*radius*radius*4)/3;
+	let val=ans.toFixed(4);
+	console.log(ans);
+	if(isNaN(ans)){
+		volume.value = `Calculated volume should be 'NaN' due to invalid input`;
+	}
+	else{
+		volume.value = `Calculated volume should be approximately ${val}`;
+	}
+	
   
 } 
 
